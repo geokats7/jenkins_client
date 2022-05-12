f5cde00e4b92b4614057e3c5da2b115a15cf48d1
Konstantinos Konstantakopoulos <konstantinos.konstantakopoulos@orfium.com>
2022-05-12 16:12:32 +0300

    style: Added precommit checks and linters. Updated style and some formatting improvements

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='jenkins_client',
+    name="jenkins_client",
     packages=find_packages(),
-    version='0.1.0',
-    description='A client for Jenkins',
-    author='Yorgos Katsaros',
-    license='MIT',
+    version="0.1.0",
+    description="A client for Jenkins",
+    author="Yorgos Katsaros",
+    license="MIT",
     long_description=readme,
     url="https://github.com/Orfium/jenkins_client",
     install_requires=["jenkinsapi", "requests"],
